951a7c2233f0fe622e3e68b7d3676acc36be0da9
Sanvi Rahman <SanviRahman>
2025-08-25 11:16:46 +0600

    Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,12 @@ import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path:'',component:HomeComponent},
-  { path:'about',component:AboutComponent},
-  { path: 'contact', component:ContactComponent},
-  { path: 'login',component:LoginComponent},
-  { path: '**',component:PageNotFoundComponent},
+  
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
